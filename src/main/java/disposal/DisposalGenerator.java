e097b78b454c2c814ff4a9a2047902271417a324
admin <ermanno.nicoletti@gmail.com>
2022-08-26 15:49:07 +0200

    Settings for rancher

--- src/main/java/disposal/DisposalGenerator.java
+++ src/main/java/disposal/DisposalGenerator.java
@@ -17,15 +17,16 @@ public class DisposalGenerator {
 
 
     public static void main(String[] args) {
-        String host = "172.16.1.3";
-        Long port = Long.valueOf(61616);
-        String queueName = "disposals2";
+        String host = "172.31.1.229";
+        Long port = Long.valueOf(32040);
+        String queueName = "disposals";
 
         String uri = "tcp://"+host+":"+port;
 
         try {
             // this code could be substituted by a lookup operation in a naming service
             QueueConnectionFactory connFactory = new ActiveMQQueueConnectionFactory(uri);
+            connFactory.createQueueConnection("artemis", "artemis");
 
             QueueConnection connection = connFactory.createQueueConnection();
             session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
@@ -34,23 +35,25 @@ public class DisposalGenerator {
             queue = session.createQueue(queueName);
             QueueSender sender = session.createSender(queue);
 
-            for(int i=0; i<1;i++){
-                DisposalDriver disposalDriver = new DisposalDriver(
-                        UUID.randomUUID().toString(),
-                        new Date(),
-                        "GENERAL_WASTE",
-                        100,
-                        UUID.fromString("ae2c98ac-0f2a-48b1-b5f0-477e5c7938bf"),  //Collection Point
-                        UUID.fromString("d95baf22-fb8c-4eae-9225-70fa8a240b67")  //Vehicle
-                );
-                String serialized = new ObjectMapper().writeValueAsString(disposalDriver);
-                TextMessage disposalMessage = session.createTextMessage(serialized);
-                sender.send(disposalMessage);
-            }
+            System.out.println();
+
+//            for(int i=0; i<1;i++){
+//                DisposalDriver disposalDriver = new DisposalDriver(
+//                        UUID.randomUUID().toString(),
+//                        new Date(),
+//                        "GENERAL_WASTE",
+//                        100,
+//                        UUID.fromString("ae2c98ac-0f2a-48b1-b5f0-477e5c7938bf"),  //Collection Point
+//                        UUID.fromString("17a6c27b-771a-4ff7-871f-c1784003cb29")  //Vehicle
+//                );
+//                String serialized = new ObjectMapper().writeValueAsString(disposalDriver);
+//                TextMessage disposalMessage = session.createTextMessage(serialized);
+//                sender.send(disposalMessage);
+//            }
 
             sender.close();
 
-        } catch (JMSException | JsonProcessingException e) {
+        } catch (JMSException /*| JsonProcessingException*/ e) {
             System.err.println("Error " + e);
         }
 
